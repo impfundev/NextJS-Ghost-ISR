8be1a6cd656b10d95f79564aad405ca430958c48
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-12 13:57:48 -0800

    Create [slug].js

---
+++ pages/tag/[slug].js
@@ -0,0 +1,64 @@
+import { gql } from "@apollo/client";
+import parse from "html-react-parser";
+
+import { client } from "../../lib/apolloClient";
+import Layout from "../../components/Layout";
+import PostsList from "../../components/PostsList";
+
+export default function SingleTag({ tag }) {
+  return (
+    <Layout>
+      <h1>{parse(tag.name)}</h1>
+      <PostsList posts={tag.posts.nodes} />
+    </Layout>
+  );
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+const GET_TAG = gql`
+  query getTag($slugId: ID!) {
+    tag(id: $slugId, idType: SLUG) {
+      name
+      posts {
+        nodes {
+          databaseId
+          title
+          excerpt
+          uri
+          featuredImage {
+            node {
+              sourceUrl
+              altText
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export async function getStaticProps(context) {
+  const { slug } = context.params;
+
+  const response = await client.query({
+    query: GET_TAG,
+    variables: { slugId: slug },
+  });
+
+  const tag = response?.data?.tag;
+
+  if (!tag) {
+    return { notFound: true };
+  }
+
+  return {
+    props: { tag },
+    revalidate: 60,
+  };
+}
