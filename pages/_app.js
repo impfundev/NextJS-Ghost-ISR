50063d813a5677490ff99dd13aa5ea543bdae3d3
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-12 14:07:16 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,14 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
     <Head>
       <meta name="robots" content="noindex" />
     </Head>
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
+    </>
   );
 }
 
