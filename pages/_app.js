3c902dceba7318482f4f2d21a1784d0ec028f70b
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-12 14:05:42 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { ApolloProvider } from "@apollo/client/react";
-
+import Head from 'next/head'
 import { client } from "../lib/apolloClient";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Head>
+      <meta name="robots" content="noindex">
+    </Head>
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
