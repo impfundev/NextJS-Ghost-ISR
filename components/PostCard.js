66b4f92e2f7d3978b417a65caaf5b1f1f5d03ceb
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-13 17:46:18 +0700

    Update PostCard.js

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,13 +3,13 @@ import parse from "html-react-parser";
 import { gql } from "@apollo/client";
 
 export default function PostCard({ post }) {
-  const { title, excerpt, uri, featuredImage } = post;
+  const { title, excerpt, slug, featuredImage } = post;
 
   return (
     <article className="max-w-sm rounded overflow-hidden shadow-lg">
       {featuredImage ? (
         <>
-          <Link href={uri}>
+          <Link href={slug}>
             <a className="w-full">
               <img
                 src={featuredImage.node.sourceUrl}
@@ -20,7 +20,7 @@ export default function PostCard({ post }) {
         </>
       ) : null}
       <h2 className="font-bold text-xl px-6 pt-4 mb-2">
-        <Link href={uri}>
+        <Link href={slug}>
           <a>{title}</a>
         </Link>
       </h2>
