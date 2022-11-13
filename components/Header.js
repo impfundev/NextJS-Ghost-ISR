69f5dbc96e6cf643d4c8776a47c94cfc2a11387d
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-13 13:06:12 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Link from "next/link";
 import Nav from "./Nav";
 
 export default function Header() {
@@ -8,12 +9,16 @@ export default function Header() {
   };
 
   return (
-    <header className="sticky top-0 bg-white flex items-center justify-between py-4">
-      <div className="text-xl font-extrabold tracking-tight">Fandomnesia</div>
+    <header className="sticky top-0 bg-white flex items-center justify-between container mx-auto px-5 py-4">
+      <Link href="/">
+        <a className="text-xl font-extrabold tracking-tight">
+          Fandomnesia
+        </a>
+      </Link>
       <button className="font-bold md:hidden" onClick={handleClick}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </button>
-      <Nav className={`${ navActive ? '' : 'hidden' } w-screen h-screen m-4 bg-white absolute md:block md:w-[auto] md:h-[auto] md:m-auto md:static`} />
+      <Nav className={`${ navActive ? '' : 'hidden' } m-4 bg-white md:block md:m-auto`} />
     </header>
   );
 }
