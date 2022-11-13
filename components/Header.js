4d3e334c5962a3220d1ff57f6641853f25014936
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-13 12:29:30 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 bg-white flex items-center justify-between py-4">
       <div className="text-xl font-extrabold tracking-tight">Fandomnesia</div>
-      <button className="font-bold" onClick={handleClick}>
+      <button className="font-bold md:hidden" onClick={handleClick}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </button>
       <Nav className={`${ navActive ? '' : 'hidden' }`} />
