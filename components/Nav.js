705f94c53f8e2367008099ab92648669fa2cf3d8
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-13 12:44:21 +0700

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,32 @@
 import Link from "next/link";
 
-export default function Nav() {
+export default function Nav({ className }) {
   return (
-    <nav>
-      <ul className="nav">
+    <nav className={className}>
+      <ul className="flex flex-col md:flex-row gap-4 items-center justify-center text-right font-bold">
         <li>
           <Link href="/">
-            <a>Home</a>
+            <a>Beranda</a>
           </Link>
         </li>
         <li>
-          <Link href="/blog">
-            <a>Blog</a>
+          <Link href="/category/budaya-populer">
+            <a>Budaya Populer</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/category/selebritis">
+            <a>Selebritis</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/category/olahraga">
+            <a>Olahraga</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/category/teknologi">
+            <a>Teknologi</a>
           </Link>
         </li>
         <li>
