ef2f197040d40ef95a3c31d62c7698bbdc62b848
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-15 21:33:04 +0700

    Update next-rss.js

--- next-rss.js
+++ next-rss.js
@@ -1,3 +1,25 @@
+import { gql } from "@apollo/client";
+import { client } from "../lib/apolloClient";
+
+const GET_POSTS = gql`
+  query getPosts {
+    posts(first: 20, after: null) {
+      nodes {
+        databaseId
+        title
+        slug
+        date
+      }
+    }
+  }
+`;
+
+const response = await client.query({
+    query: GET_POSTS,
+});
+
+const posts = response.data.posts.nodes;
+
 module.exports = {
     siteTitle: 'example web site',
     siteDescription: 'example web site rss feed',
@@ -6,10 +28,10 @@ module.exports = {
     siteUrl: 'http://example.com',
     outDir: 'public',
     postsDir: '',
-    createFeedItem: (pageProps) => ({
-        title: pageProps.postData.title,
-        id: `${config.siteUrl}/${config.postsDir}/${pageProps.postData.id}`,
-        link: `${config.siteUrl}/${config.postsDir}/${pageProps.postData.id}`,
-        date: new Date(pageProps.postData.date)
+    createFeedItem: ({ posts }) => ({
+        title: posts.map((post) => `${post.title}`),
+        id: posts.map((post) => `${post.databaseId}`),
+        link: posts.map((post) => `${post.slug}`),
+        date: new Date(posts.map((post) => `${post.date}`))
     }),
 }
