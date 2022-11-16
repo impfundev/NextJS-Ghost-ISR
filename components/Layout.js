641d025972814e0cb617e5554047c76ba717e887
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-16 22:06:34 +0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,8 @@
-import Head from "next/head";
-
 import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Head>
-        <title>Fandomnesia</title>
-      </Head>
       <Header />
       <main className="container mx-auto px-5 pb-8">
         {children}
