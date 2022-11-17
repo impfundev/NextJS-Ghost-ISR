721194d0c4266ec722f7497994727a8bf9d07375
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-17 15:35:32 +0700

    Update amp.js

--- pages/[slug]/amp.js
+++ pages/[slug]/amp.js
@@ -85,6 +85,125 @@ font-weight: 700;
 border-radius: 9999px;
 }
 
+/* Main Content */
+iframe {
+  width: 100%;
+  height: auto;
+  min-height: 300px;
+}
+
+a {
+  text-decoration: none;
+}
+
+.has-text-align-left {
+  text-align: left;
+}
+
+.has-text-align-center {
+  text-align: center;
+}
+
+.has-text-align-right {
+  text-align: right;
+}
+
+.has-large-font-size {
+  font-size: 2.25rem;
+  line-height: 2.5rem; 
+}
+
+.alignfull {
+  position: relative;
+  width: 100vw;
+  left: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+  max-width: 100vw;
+  right: 50%;
+}
+
+.wp-block-image img {
+margin-top: 0.5rem; 
+max-width: 100%;
+}
+.wp-block-image.aligncenter {
+text-align: center;
+}
+.wp-block-image.alignfull img,
+.wp-block-image.alignwide img {
+  width: 100%;
+}
+.wp-block-image .alignleft,
+.wp-block-image .alignright,
+.wp-block-image .aligncenter,
+.wp-block-image.is-resized {
+display: table;
+margin-right: 0;
+margin-left: 0;
+}
+.wp-block-image .alignleft > figcaption,
+.wp-block-image .alignright > figcaption,
+.wp-block-image .aligncenter > figcaption,
+.wp-block-image.is-resized > figcaption {
+  display: table-caption;
+  caption-side: bottom;
+}
+
+.wp-block-image .alignleft {
+float: left; 
+margin-right: 1rem;
+}
+.wp-block-image .alignright {
+float: right; 
+margin-left: 1rem;
+}
+.wp-block-image .aligncenter {
+  margin: auto;
+}
+
+.wp-block-button a,
+.wp-block-file a.wp-block-file__button {
+padding-top: 0.5rem;
+padding-bottom: 0.5rem; 
+padding-left: 1rem;
+padding-right: 1rem; 
+background-color: #000000; 
+color: #ffffff; 
+text-decoration: none;
+}
+
+.wp-block-button a:hover,
+.wp-block-file a.wp-block-file__button:hover {
+background-color: #000000; 
+cursor: pointer;
+}
+
+.wp-block-file a:first-of-type {
+  margin-right: 1rem; 
+}
+
+.wp-block-cover {
+display: flex; 
+overflow: hidden; 
+background-position: center; 
+background-size: cover; 
+flex-wrap: wrap; 
+justify-content: center; 
+align-items: center;
+min-height: 430px;
+}
+
+.wp-block-verse {
+font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; 
+}
+
+.wp-block-media-text {
+display: grid; 
+grid-template-columns: repeat(2, minmax(0, 1fr)); 
+gap: 1rem;
+}
+
 </style>
     </Head>
     <header className="header">
