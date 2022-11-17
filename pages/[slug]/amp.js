fe6657654241bf0cdbd24bb3cc5d32c41830abac
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-17 15:21:35 +0700

    Update amp.js

--- pages/[slug]/amp.js
+++ pages/[slug]/amp.js
@@ -19,21 +19,88 @@ export default function SinglePost({ item }) {
     <Head>
       <title>{title}</title>
       <meta name="description" content={parse(excerpt)} />
+<style jsx global>
+.header {
+position: sticky; 
+top: 0; 
+background-color: #ffffff;
+}
+
+.header-wrapper {
+width: 100%; 
+display: flex; 
+padding-top: 1rem;
+padding-bottom: 1rem; 
+padding-left: 1.25rem;
+padding-right: 1.25rem; 
+justify-content: space-between; 
+align-items: center;
+}
+
+.logo-wrapper {
+display: flex; 
+align-items: center; 
+gap: 1rem;
+}
+
+.logo {
+border-radius: 9999px;
+}
+
+.site-title {
+font-size: 1.125rem;
+line-height: 1.75rem; 
+font-weight: 700;
+}
+
+.menu-icon {
+display: inline-block; 
+width: 1.5rem; 
+height: 1.5rem; 
+stroke: currentColor;
+}
+
+.main-wrapper {
+width: 100%; 
+padding-left: 1.25rem;
+padding-right: 1.25rem; 
+padding-bottom: 2rem;
+}
+
+.category-wrapper {
+padding-top: 1rem;
+padding-bottom: 1rem;
+}
+
+.category {
+padding-top: 0.25rem;
+padding-bottom: 0.25rem; 
+padding-left: 0.75rem;
+padding-right: 0.75rem; 
+background-color: #000000; 
+color: #ffffff; 
+font-size: 0.875rem;
+line-height: 1.25rem; 
+font-weight: 700; 
+border-radius: 9999px;
+}
+
+</style>
     </Head>
-    <header className="sticky top-0 bg-white/75 md:bg-white backdrop-blur-3xl">
-      <div className="flex items-center justify-between container mx-auto px-5 py-4">
+    <header className="header">
+      <div className="header-wrapper">
         <Link href="/">
-          <a className="flex items-center gap-4">
-            <svg className="rounded-full" version="1.0" xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 152.000000 152.000000" preserveAspectRatio="xMidYMid meet">
+          <a className="logo-wrapper">
+            <svg className="logo" version="1.0" xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 152.000000 152.000000" preserveAspectRatio="xMidYMid meet">
               <g transform="translate(0.000000,152.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none"><path d="M0 760 l0 -760 760 0 760 0 0 760 0 760 -760 0 -760 0 0 -760z m699 519 c35 -5 107 -30 160 -54 110 -50 168 -63 210 -46 28 12 51 47 51 77 0 25 35 12 38 -14 7 -46 -19 -159 -49 -210 -80 -140 -243 -150 -439 -28 -117 74 -144 86 -185 86 -66 0 -98 -65 -52 -108 12 -11 52 -34 89 -50 79 -36 144 -92 153 -133 l7 -29 124 0 c102 0 124 -3 124 -15 0 -12 -23 -15 -122 -17 l-123 -3 -3 -217 -2 -218 -155 0 -155 0 0 374 c0 344 2 379 20 437 29 97 87 150 183 169 58 11 55 11 126 -1z"/></g>
             </svg>
-            <span className="text-lg font-bold">
+            <span className="site-title">
               Fandomnesia
             </span>
           </a>
         </Link>
-        <button className="md:hidden" on='tap:sidebar1.toggle'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        <button on='tap:sidebar1.toggle'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="menu-icon"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
         </button>
       </div>
       <amp-sidebar id="sidebar1" layout="nodisplay" side="right">
@@ -65,15 +132,15 @@ export default function SinglePost({ item }) {
       </amp-sidebar>
     </header>
 
-    <main className="container mx-auto px-5 pb-8">
+    <main className="main-wrapper">
       <>
         {haveCategories ? (
           <>
             {categories.nodes.map((category) => {
               const { slug, name } = category;
               return (
-                <div className="py-4" key={slug}>
-                  <a href={`/category/${slug}`} className="px-3 py-1 bg-black text-white text-sm font-bold rounded-full">
+                <div className="category-wrapper" key={slug}>
+                  <a href={`/category/${slug}`} className="category">
                     {name}
                   </a>
                 </div>
