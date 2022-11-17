92ee1ec445651f0056d5235558ff0d1c47454d22
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-17 16:27:42 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
-import Head from 'next/head'
-import "../styles/globals.css";
+import Head from 'next/head';
+import { useAmp } from 'next/amp';
+
+import AMPCSS from '../components/AMPCSS';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const isAmp = useAmp();
+
   return (
     <>
     <Head>
       <meta name="robots" content="noindex" />
+      {isAmp ? (
+        <AMPCSS />
+      ) : null}
     </Head>
     <Component {...pageProps} />
     </>
