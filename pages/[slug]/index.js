6cd162c7c4f5d96a5faaeb87b6cc7ec3af1d5b69
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-20 16:07:23 +0700

    Update index.js

--- pages/[slug]/index.js
+++ pages/[slug]/index.js
@@ -175,7 +175,7 @@ const GET_POST = gql`
 `;
 
 const GET_RELATED = gql`
-  query getRelated($catSlug: ID!, $postId: TermObjects) {
+  query getRelated($catSlug: ID!, $postId: ID!) {
     category(id: $catSlug, idType: SLUG) {
       posts(where: { notIn: $postId }) {
         nodes {
