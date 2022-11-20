16c1d93200860d4f1998ad7b7384c71ea851d7c7
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-20 20:47:12 +0700

    Create privacy-policy.js

---
+++ pages/privacy-policy.js
@@ -0,0 +1,42 @@
+import Layout from "../components/Layout";
+
+export default function Privacy() {
+  
+  return (
+    <>
+    <Layout>
+      <article className="prose lg:prose-xl">
+        <h1 className="text-xl">Kebijakan Privasi</h1>
+
+<h2>Siapa kami</h2>
+<p>Fandomnesia adalah portal informasi yang menyajikan berita terkini serta fakta menarik mengenai Budaya Populer, Selebritis, Gaya Hidup, dan Olahraga yang dapat diakses melalui <strong>www.fandomnesia.com</strong>.</p>
+<h2>Komentar</h2>
+<p>Ketika pengunjung meninggalkan komentar di situs, kami mengumpulkan data yang ditampilkan dalam formulir komentar, dan juga alamat IP pengunjung dan string agen pengguna browser untuk membantu deteksi spam.</p>
+<p>String anonim yang dibuat dari alamat email Anda (juga disebut hash) dapat diberikan ke layanan Gravatar untuk melihat apakah Anda menggunakannya. Kebijakan privasi layanan Gravatar tersedia di sini: https://automattic.com/privacy/. Setelah komentar Anda disetujui, gambar profil Anda dapat dilihat oleh publik dalam konteks komentar Anda.</p>
+<h2>Media</h2>
+<p>Jika Anda mengunggah gambar ke situs web, Anda harus menghindari mengunggah gambar dengan data lokasi tertanam (GPS EXIF) disertakan. Pengunjung situs web dapat mengunduh dan mengekstrak data lokasi apa pun dari gambar di situs web.</p>
+<h2>Cookies</h2>
+<p>Jika Anda meninggalkan komentar di situs kami, Anda dapat memilih untuk menyimpan nama, alamat email, dan situs web Anda dalam cookie. Ini untuk kenyamanan Anda sehingga Anda tidak perlu mengisi rincian Anda lagi ketika Anda meninggalkan komentar lain. Cookies ini akan bertahan selama satu tahun.</p>
+<p>Jika Anda mengunjungi halaman login kami, kami akan menetapkan cookie sementara untuk menentukan apakah browser Anda menerima cookie. Cookie ini tidak mengandung data pribadi dan dibuang saat Anda menutup browser Anda.</p>
+<p>Saat Anda masuk, kami juga akan menyiapkan beberapa cookie untuk menyimpan informasi masuk Anda dan pilihan tampilan layar Anda. Cookie login bertahan selama dua hari, dan cookie opsi layar bertahan selama satu tahun. Jika Anda memilih "Ingat Saya", login Anda akan bertahan selama dua minggu. Jika Anda logout dari akun Anda, cookie login akan dihapus.</p>
+<p>Jika Anda mengedit atau menerbitkan artikel, cookie tambahan akan disimpan di browser Anda. Cookie ini tidak menyertakan data pribadi dan hanya menunjukkan ID posting dari artikel yang baru saja Anda edit. Kadaluarsa setelah 1 hari.</p>
+<h3>Tambahan</h3>
+<p>Situs ini menggunakan caching untuk memfasilitasi waktu respons yang lebih cepat dan pengalaman pengguna yang lebih baik. Caching berpotensi menyimpan salinan duplikat dari setiap halaman web yang ditampilkan di situs ini. Semua file cache bersifat sementara, dan tidak pernah diakses oleh pihak ketiga mana pun, kecuali jika diperlukan untuk mendapatkan dukungan teknis dari vendor plugin cache. File cache kedaluwarsa pada jadwal yang ditetapkan oleh administrator situs, tetapi dapat dengan mudah dihapus oleh admin sebelum kedaluwarsa alami, jika perlu. Kami mungkin menggunakan layanan QUIC.cloud untuk memproses &amp; menyimpan data Anda sementara. Silakan lihat <strong>https://quic.cloud/privacy-policy/</strong> untuk detail lebih lanjut.</p>
+<h2>Konten yang disematkan dari situs web lain</h2>
+<p>Artikel di situs ini dapat menyertakan konten yang disematkan (misalnya video, gambar, artikel, dll.). Konten yang disematkan dari situs web lain berperilaku dengan cara yang sama persis seperti jika pengunjung mengunjungi situs web lain.</p>
+<p>Situs web ini dapat mengumpulkan data tentang Anda, menggunakan cookie, menyematkan pelacakan pihak ketiga tambahan, dan memantau interaksi Anda dengan konten yang disematkan, termasuk melacak interaksi Anda dengan konten yang disematkan jika Anda memiliki akun dan masuk ke situs web tersebut.</p>
+<h2>Dengan siapa kami membagikan data Anda</h2>
+<p>Jika Anda meminta pengaturan ulang kata sandi, alamat IP Anda akan disertakan dalam email pengaturan ulang.</p>
+<h2>Berapa lama kami menyimpan data Anda</h2>
+<p>Jika Anda meninggalkan komentar, komentar dan metadatanya akan disimpan tanpa batas waktu. Ini agar kami dapat mengenali dan menyetujui komentar tindak lanjut apa pun secara otomatis alih-alih menahannya dalam antrean moderasi.</p>
+<p>Untuk pengguna yang mendaftar di situs web kami (jika ada), kami juga menyimpan informasi pribadi yang mereka berikan di profil pengguna mereka. Semua pengguna dapat melihat, mengedit, atau menghapus informasi pribadi mereka kapan saja (kecuali mereka tidak dapat mengubah nama pengguna). Administrator situs web juga dapat melihat dan mengedit informasi tersebut.</p>
+<h2>Hak apa yang Anda miliki atas data Anda?</h2>
+<p>Jika Anda memiliki akun di situs ini, atau telah meninggalkan komentar, Anda dapat meminta untuk menerima file yang diekspor dari data pribadi yang kami miliki tentang Anda, termasuk data apa pun yang telah Anda berikan kepada kami. Anda juga dapat meminta kami menghapus data pribadi apa pun yang kami miliki tentang Anda. Ini tidak termasuk data apa pun yang wajib kami simpan untuk tujuan administratif, hukum, atau keamanan.</p>
+<h2>Ke mana data Anda dikirim</h2>
+<p>Komentar pengunjung dapat diperiksa melalui layanan deteksi spam otomatis.</p>
+
+      </article>
+    </Layout>
+    </>
+  );
+}
