92d89038baf4ef8997ceee88a3edbc1a26e17450
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-20 20:42:08 +0700

    Update and rename privacy-policy.js to disclaimer.js

---
+++ pages/disclaimer.js
@@ -0,0 +1,23 @@
+import Layout from "../components/Layout";
+
+export default function SinglePost() {
+  
+  return (
+    <>
+    <Layout>
+      <article className="prose lg:prose-xl">
+        <h1 className="text-xl">Disclaimer</h1>
+
+<p>Seluruh layanan yang diberikan mengikuti aturan main yang berlaku dan ditetapkan oleh Fandomnesia.</p>
+<p><strong>Disclaimer</strong></p>
+<p>Seluruh layanan yang diberikan di situs Fandomnesia ditujukan untuk kebutuhan pribadi Anda dan bukan untuk digunakan kembali secara komersial. Seluruh konten yang ada di dalam situs termasuk teks, video, foto, audio, ilustrasi, infografik dan lainnya dilindungi oleh undang-undang hak cipta yang dimiliki Fandomnesia, dan atau pihak ketiga penyedia isi di situs ini.</p>
+<p>Fandomnesia berhak memuat, atau tidak memuat, melakukan penyuntingan atau menghapus data atau informasi dari pembaca.</p>
+<p>Fandomnesia tak bertanggungjawab atas kegagalan penyampaian data atau informasi dari pembaca melalui berbagai saluran komunikasi, baik email, SMS, atau jalur komunikasi online lainnya akibat kesalahan teknis yang tak diharapkan.</p>
+<p>Segala bentuk data atau informasi yang disiarkan oleh Fandomnesia adalah sebagai rujukan dan tidak diharapkan untuk tujuan komersial lain semisal perdagangan saham, transaksi bisnis, keuangan, dan transaksi lainnya.</p>
+<p>Data dan informasi yang disajikan oleh Fandomnesia atau mitra penyedia isi, diupayakan memenuhi akurasi yang secermat mungkin. Meski begitu, Fandomnesia tidak bertanggungjawab atas kesalahan atau keterlambatan memperbarui data, atau segala kerugian yang timbul karena tindakan penggunaan data dan atau informasi yang disajikan di situs ini.</p>
+
+      </article>
+    </Layout>
+    </>
+  );
+}
