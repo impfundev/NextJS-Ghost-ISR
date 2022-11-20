5fe0d3c732cd18c0b71b5734ce0eb83c73e10169
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-20 20:50:02 +0700

    Create tentang-kami.js

---
+++ pages/tentang-kami.js
@@ -0,0 +1,21 @@
+import Layout from "../components/Layout";
+
+export default function Tentang() {
+  
+  return (
+    <>
+    <Layout>
+      <article className="prose lg:prose-xl">
+        <h1 className="text-xl">Tentang Kami</h1>
+
+<p>Seiring meningkatnya minat banyak orang dengan dunia hiburan, kebutuhan akan informasi terkait apa yang mereka sukai juga meningkat. Disinilah Fandomnesia hadir untuk memfasilitasi banyak orang dengan informasi yang terpercaya.</p>
+<p>Fandomnesia menyajikan informasi terkini terkait dunia hiburan untuk sektor Budaya Populer yang meliputi Film, Serial Televisi, Animasi, Komik dan lainya.</p>
+<p>Kami juga membagikan informasi yang meliputi dunia para selebriti tanah air maupun luar negeri, dengan fakta berdasarkan sumber terpercaya.</p>
+<p>Terakhir, kami juga membagikan informasi dan fakta menarik terkait dunia olahraga meliputi Sepak Bola, Basket, Bulutangkis dan lainya, yang dimana kini sudah menjadi bagian dari dunia hiburan.</p>
+<p>Kepercayaan para pengunjung platform kami adalah prioritas utama, kami hadir untuk menyajikan informasi dengan bersih, <em>No Hoax</em>.</p>
+
+      </article>
+    </Layout>
+    </>
+  );
+}
