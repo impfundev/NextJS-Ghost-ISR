e122f1f4931fc509e92b16e7a6f99b01313ba9d5
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-20 21:12:47 +0700

    Create [slug].js

---
+++ pages/author/[slug].js
@@ -0,0 +1,77 @@
+import { gql } from "@apollo/client";
+
+import { client } from "../../lib/apolloClient";
+import Layout from "../../components/Layout";
+import PostsList from "../../components/PostsList";
+
+export default function Author({ user }) {
+  return (
+    <Layout>
+      <h1 className="py-6 text-lg font-bold">By {user.name}</h1>
+      <PostsList posts={user.posts.nodes} />
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const GET_USERSLUG = gql`
+    query getUserSlug {
+      users {
+        nodes {
+          slug
+        }
+      }
+    }
+  `;
+
+  const { data } = await client.query({
+    query: GET_USERSLUG,
+  });
+
+  return {
+    paths: data?.users.nodes.map((author) => `/author/${author.slug}` ) || [],
+    fallback: "blocking",
+  };
+}
+
+const GET_USER = gql`
+  query getUser($slugId: ID!) {
+    user(id: $slugId, idType: SLUG) {
+      name
+      posts {
+        nodes {
+          databaseId
+          title
+          excerpt
+          slug
+          featuredImage {
+            node {
+              sourceUrl
+              altText
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export async function getStaticProps(context) {
+  const { slug } = context.params;
+
+  const response = await client.query({
+    query: GET_USER,
+    variables: { slugId: slug },
+  });
+
+  const user = response?.data?.user;
+
+  if (!user) {
+    return { notFound: true };
+  }
+
+  return {
+    props: { user },
+    revalidate: 60,
+  };
+}
