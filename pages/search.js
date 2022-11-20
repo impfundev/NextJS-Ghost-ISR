02ae0a8f6b08f65cfdbcf4deb0a779e2923f2725
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-20 20:38:04 +0700

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -5,9 +5,9 @@ export default function Search() {
     <>
     <Layout>
       <article className="prose lg:prose-xl">
-        <h1>Temukan Berita atau Konten Menarik yang cari disini</h1>
+        <h1 className="text-xl">Temukan Berita atau Konten Menarik yang cari disini</h1>
         <script async src="https://cse.google.com/cse.js?cx=fd73c201cbd659445"></script>
-        <div className="gcse-search"></div>
+        <div className="gcse-search my-4"></div>
       </article>
     </Layout>
     </>
