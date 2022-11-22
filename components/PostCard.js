5d12d7f84f2f0cc2133dce923180eeaa3cddb748
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-22 05:31:31 -0800

    Update PostCard.js

--- components/PostCard.js
+++ components/PostCard.js
@@ -5,14 +5,8 @@ export default function PostCard({ post }) {
   const { title, excerpt, slug, featuredImage } = post;
 
   return (
-    <article className="max-w-sm border-t border-inherit">
-      <h2 className="font-bold text-lg md:text-xl pt-1 pb-4">
-        <a href={`/${slug}`}>
-          {title}
-        </a>
-      </h2>
+    <article className="max-w-sm rounded-2xl shadow-lg overflow-hidden border border-black text-black hover:border-yellow-500 hover:text-yellow-500 transition-all duration-300">
       <a href={`/${slug}`}>
-        <div className="text-gray-700 text-sm md:text-base mb-4">{parse(excerpt)}</div>
         {featuredImage ? (
           <>
             <img
@@ -23,6 +17,14 @@ export default function PostCard({ post }) {
           </>
         ) : null}
       </a>
+      <div className="grid gap-4 m-5">
+        <h2 className="font-bold text-lg md:text-xl pt-1 pb-4">
+          <a href={`/${slug}`}>
+            {title}
+          </a>
+        </h2>
+        <a href={`/${slug}`}>{parse(excerpt)}</a>
+      </div>
     </article>
   );
 }
