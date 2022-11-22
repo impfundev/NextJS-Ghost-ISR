9b32f7051918ff9b1b69f53948246580ad11887c
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-22 05:28:54 -0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -7,16 +7,16 @@ export default function Layout({ children }) {
       <main className="container mx-auto px-5 pb-8">
         {children}
       </main>
-<footer className="bg-gray-300">
+<footer className="bg-yellow-200 border-t border-black">
     <div className="grid grid-cols-2 gap-8 py-8 px-6 md:grid-cols-4">
         <div>
             <h2 className="mb-6 text-sm font-semibold uppercase text-gray-500">Situs</h2>
             <ul>
                 <li className="mb-4">
-                    <a href="/tentang-kami/" className="hover:underline">Tentang Kami</a>
+                    <a href="/tentang-kami/" className="hover:font-bold transition-all duration-300">Tentang Kami</a>
                 </li>
                 <li className="mb-4">
-                    <a href="/tentang-kami/" className="hover:underline">Kontak Kami</a>
+                    <a href="/tentang-kami/" className="hover:font-bold transition-all duration-300">Kontak Kami</a>
                 </li>
             </ul>
         </div>
@@ -24,16 +24,16 @@ export default function Layout({ children }) {
             <h2 className="mb-6 text-sm font-semibold uppercase text-gray-500">Channel</h2>
             <ul>
                 <li className="mb-4">
-                    <a href="#" className="hover:underline">YouTube</a>
+                    <a href="#" className="hover:font-bold transition-all duration-300">YouTube</a>
                 </li>
                 <li className="mb-4">
-                    <a href="https://twitter.com/fandomnesia_com" className="hover:underline">Twitter</a>
+                    <a href="https://twitter.com/fandomnesia_com" className="hover:font-bold transition-all duration-300">Twitter</a>
                 </li>
                 <li className="mb-4">
-                    <a href="#" className="hover:underline">Facebook</a>
+                    <a href="#" className="hover:font-bold transition-all duration-300">Facebook</a>
                 </li>
                 <li className="mb-4">
-                    <a href="https://news.google.com/publications/CAAqBwgKMK2zowsw8L27Aw" className="hover:underline">Google News</a>
+                    <a href="https://news.google.com/publications/CAAqBwgKMK2zowsw8L27Aw" className="hover:font-bold transition-all duration-300">Google News</a>
                 </li>
             </ul>
         </div>
@@ -41,13 +41,13 @@ export default function Layout({ children }) {
             <h2 className="mb-6 text-sm font-semibold uppercase text-gray-500">Legal</h2>
             <ul>
                 <li className="mb-4">
-                    <a href="/disclaimer/" className="hover:underline">Disclaimer</a>
+                    <a href="/disclaimer/" className="hover:font-bold transition-all duration-300">Disclaimer</a>
                 </li>
                 <li className="mb-4">
-                    <a href="/privacy-policy/" className="hover:underline">Kebijakan Privasi</a>
+                    <a href="/privacy-policy/" className="hover:font-bold transition-all duration-300">Kebijakan Privasi</a>
                 </li>
                 <li className="mb-4">
-                    <a href="/pedoman-media-siber/" className="hover:underline">Pedoman Media Siber</a>
+                    <a href="/pedoman-media-siber/" className="hover:font-bold transition-all duration-300">Pedoman Media Siber</a>
                 </li>
             </ul>
         </div>
