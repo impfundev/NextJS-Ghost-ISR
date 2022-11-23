eec96f1089f0732fbaa526e247d5860175e4fb65
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-24 01:04:32 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} \*/
 module.exports = {
   content: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
@@ -27,6 +29,9 @@ module.exports = {
       boxShadow: {
         sm: '0 5px 10px rgba(0, 0, 0, 0.12)',
         md: '0 8px 30px rgba(0, 0, 0, 0.12)',
+      },
+      fontFamily: {
+        sans: ['var(--rubik-font)', ...fontFamily.sans],
       },
     },
   },
