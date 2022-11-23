b61c6c90df2d1ea4a674e1cf696a702b36495f0e
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-23 21:30:10 +0700

    Update PostsList.js

--- components/PostsList.js
+++ components/PostsList.js
@@ -4,7 +4,7 @@ import PostCard from "./PostCard";
 
 export default function PostsList({ posts }) {
   return (
-    <ul className="grid md:grid-cols-2 gap-3 py-5">
+    <ul className="grid md:grid-cols-2 gap-6 py-5">
       {posts.map((post) => {
         return (
           <li key={post.databaseId}>
