96762d08edc2f2c80262ba256438265abc1efdc4
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-24 18:47:21 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
 import Head from 'next/head';
-import { Rubik } from '@next/font/google';
 import '../styles/globals.css';
-
-  const rubik = Rubik({
-    weight: ['400', '700'],
-    style: ['normal', 'italic'],
-    subsets: ['latin'],
-    variable: '--rubik-font',
-  });
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -15,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     <Head>
       <meta name="robots" content="noindex" />
     </Head>
-    <Component {...pageProps} className={rubik.variable} />
+    <Component {...pageProps} />
     </>
   );
 }
