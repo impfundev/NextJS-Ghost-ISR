47078b066afe59241cdc65103b7f5de5091fcb16
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-24 23:14:31 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
-import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <meta name="robots" content="noindex" />
-    </Head>
     <Component {...pageProps} />
     </>
   );
