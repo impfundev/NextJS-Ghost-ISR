febcdc46d1e30cd0c98e5d58ebb83a5669d99001
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-24 23:29:35 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,40 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { useAmp } from 'next/amp';
-import ampify from 'ampify';
 
 export default function Document() {
-  const isAmp = useAmp();
-  const rawHtml = `<Html lang="id">
-        <Head>
-          <meta name="robots" content="noindex" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>`;
-  const amp = ampify(html, {
-    cwd: 'amp',
-    canonicalURL: 'https://fandomnesia.vercel.app',
-  });
-
   return (
-    <>
-      {isAmp ? (
-        <>
-          { amp }
-        </>
-      ) : (
-      <Html lang="id">
-        <Head>
-          <meta name="robots" content="noindex" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-      )}
-    </>
+    <Html lang="id">
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
   )
 }
