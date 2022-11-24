a38b28ae8a61c2fe1d211587658183b6bde8a590
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-24 20:48:41 +0700

    Update layout.js

--- app/[slug]/layout.js
+++ app/[slug]/layout.js
@@ -1,9 +1,14 @@
 export default function Layout({ children }) {
   return (
-    <html lang="id">
+    <html lang="id" amp="amp">
       <head>
-        <title>Next.js</title>
-
+        <title>Amp Post demo</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="canonical" href="/article.html" />
+        <link rel="shortcut icon" href="favicon.ico"/>
+        <script async src="https://cdn.ampproject.org/v0.js"></script>
+        <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 <style amp-custom="amp-custom">{`
 .header {
 position: sticky; 
