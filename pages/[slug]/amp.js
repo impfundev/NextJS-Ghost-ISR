a1451b210e23a49692bd6a3848bb370be6b7d7d9
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-25 14:51:22 +0700

    Update amp.js

--- pages/[slug]/amp.js
+++ pages/[slug]/amp.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 import parse from "html-react-parser";
 import date from "date-and-time";
-import Router from "next/router";
+import { useRouter } from "next/router";
 
 import { client } from "../../lib/apolloClient";
 
