1a3a4dd49cf568031337ce5f2f87061929ecddb4
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-25 15:05:39 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  images: {
-    domains: ['cmsnya.fandomnesia.com'],
-    formats: ['image/avif', 'image/webp'],
+  experimental: {
+    amp: {
+      skipValidation: true
+    },
   },
 }
