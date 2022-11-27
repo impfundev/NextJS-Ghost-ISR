b90a807ec0adefbb83ee62fc2db2026aa234024a
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-27 16:36:53 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apolloClient";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }, context) {
+  const apolloClient = useApollo(pageProps);
+
   return (
     <>
-    <Head>
-      <meta name="robots" content="noindex" />
-    </Head>
-    <Component {...pageProps} />
+    <ApolloProvider client={apolloClient}>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
     </>
   );
 }
