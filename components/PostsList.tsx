d8a3efc520d01b620516fdcb27fbc81351f4919f
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-27 22:17:35 +0700

    Update PostsList.tsx

--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -7,10 +7,26 @@ interface Props {
   [key: string]: any;
 };
 
+interface Post {
+  post: {
+    title: string;
+    excerpt: string;
+    slug: string;
+    featuredImage: {
+      node: {
+        sourceUrl: string;
+        altText: string;
+        srcSet: string;
+        sizes: string;
+      }
+    }
+  }
+};
+
 export default function PostsList({ posts }: Props) {
   return (
     <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 py-5">
-      {posts.map((post: Props) => {
+      {posts.map((post: Post) => {
         return (
           <li key={post.databaseId}>
             <PostCard post={post} />
