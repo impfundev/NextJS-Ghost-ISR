d09a9856022a4237a71569a181ac08c40723ad5e
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-27 17:58:02 +0700

    Update feed.xml.js

--- pages/feed.xml.js
+++ pages/feed.xml.js
@@ -1,88 +1,102 @@
-import { ApolloClient, createHttpLink, InMemoryCache, gql } from "@apollo/client";
-import RSS from 'rss';
+import { gql, useQuery } from "@apollo/client";
+import { GetStaticPropsContext } from "next";
+
+import { initializeApollo, addApolloState } from "../lib/TsApolloClient";
+
+interface Post {
+  databaseId: number;
+  title: string;
+};
+
+interface PostEdge {
+  node: Post;
+};
+
+const POSTS_PER_PAGE = 20;
 
 const GET_POSTS = gql`
-  query getPosts {
-    posts(first: 20, after: null) {
-      nodes {
-        title
-        slug
-        excerpt
-        featuredImage {
-          node {
-            sourceUrl
-          }
+  query getPosts($first: Int!, $after: String) {
+    posts(first: $first, after: $after) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      edges {
+        node {
+          id
+          databaseId
+          title
+          slug
+          date
+          excerpt
+          content
         }
       }
     }
   }
 `;
 
-export async function getServerSideProps({ req, res }) {
-  const client = new ApolloClient({
-    ssrMode: true,
-    link: createHttpLink({
-      uri: 'https://cmsnya.fandomnesia.com/graphql',
-      credentials: 'same-origin',
-      headers: {
-        cookie: req.header('Cookie'),
-      },
-    }),
-    cache: new InMemoryCache(),
-  });
-  const response = await client.query({
+export async function getServerSideProps(context: GetStaticPropsContext) {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
     query: GET_POSTS,
+    variables: {
+      first: POSTS_PER_PAGE,
+      after: null,
+    }
   });
-  const posts = response.data.posts.nodes;
-  const siteUrl = 'https://fandomnesia-react.vercel.app';
-  
-  const feedOptions = {
-    title: 'Fandomnesia',
-    description: 'Fandomnesia.com menyajikan berita terbaru dan konten menarik seputar budaya populer, selebritis, teknologi, dan olahraga.',
-    feed_url: `${siteUrl}/feed.xml`,
-    site_url: siteUrl,
-    image_url: `${siteUrl}/favicon.jpg`,
-    categories: ['Budaya Populer','Selebritis','Olahraga','Teknologi'],
-    custom_namespaces: {
-      'media': 'http://search.yahoo.com/mrss/'
-    },
-  };
-  const feed = new RSS(feedOptions);
-  const results = posts.map((post) => {
-          feed.item({
-            title: post.title,
-            description: post.excerpt,
-            url: `${siteUrl}/${post.slug}`,
-            date: post.date,
-            custom_elements: [
-              {'media:content': {
-                _attr: {
-                  url: post.featuredImage?.node.sourceUrl || `${siteUrl}/favicon.jpg`,
-                  type: 'image/jpeg',
-                  medium: 'image',
-                }
-              }},
-            ],
-          });
-        });
-  await results;
 
   //Set page headers
   res.setHeader("Content-Type", "text/xml; charset=utf-8");
-  //Set cache for 60s so it wont call our wp on every request.
-  res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
   res.write(Feeds(feed));
   res.end();
 
-  return {
-    props: {}
-  };
+  return addApolloState(apolloClient, {
+    props: {},
+  });
 }
 
 export default function Feed() {
+  const { loading, error, data } = useQuery(GET_POSTS, {
+    variables: {
+      first: POSTS_PER_PAGE,
+      after: null,
+    }
+  });
+  const posts = data?.posts?.edges?.map((edge: PostEdge) => edge.node) || [];
+  const siteUrl = 'https://fandomnesia-react.vercel.app';
 
-  return (
-    <>
-    </>
-  );
+  return `<?xml version="1.0" ?>
+        <rss
+          xmlns:dc="http://purl.org/dc/elements/1.1/"
+          xmlns:content="http://purl.org/rss/1.0/modules/content/"
+          xmlns:atom="http://www.w3.org/2005/Atom"
+          version="2.0"
+        >
+          <channel>
+              <title>Fandomnesia</title>
+              <atom:link href="${siteUrl}/feed.xml" rel="self" type="application/rss+xml" />
+              <link>${siteUrl}</link>
+              <description>Your site description</description>
+              <language>id-ID</language>
+              ${posts.map((post: Post) => {
+                return (
+                  <lastBuildDate>${post.date}</lastBuildDate>
+                  <item>
+                    <title><![CDATA[${post.title}]]></title>
+                    <link>${siteUrl}/${post.slug}</link>
+                    <pubDate>${post.date}</pubDate>
+                    <guid isPermaLink="false">${siteUrl}/${post.slug}</guid>
+                    <description>
+                      <![CDATA[${post.excerpt}]]>
+                    </description>
+                    <content:encoded>
+                      <![CDATA[${post.content}]]>
+                    </content:encoded>
+                  </item>
+                );
+              }
+          </channel>
+        </rss>`;
 }
