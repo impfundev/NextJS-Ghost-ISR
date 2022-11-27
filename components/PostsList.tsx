cdb03bc85e848925c7fbfbe605a12ab385c2f4a7
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-27 22:40:18 +0700

    Update PostsList.tsx

--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -27,9 +27,35 @@ export default function PostsList({ posts }: Props) {
   return (
     <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 py-5">
       {posts.map((post: Post) => {
-        return (
-          <PostCard post={post} />
-        );
+  const { title, excerpt, slug, featuredImage } = post;
+
+  return (
+  <li key={slug}>
+    <article className="max-w-sm rounded-2xl shadow-lg overflow-hidden border border-black text-black hover:border-yellow-500 hover:text-yellow-500 transition-all duration-300">
+      <a className="relative" href={`/${slug}`} aria-label={title}>
+        {featuredImage ? (
+          <>
+            <img
+              className="w-full object-cover h-60"
+              src={featuredImage.node.sourceUrl}
+              alt={featuredImage.node.altText}
+              srcSet={featuredImage.node.srcSet}
+              sizes={featuredImage.node.sizes}
+            />
+          </>
+        ) : null}
+      </a>
+      <div className="grid gap-4 m-5">
+        <h2 className="font-bold text-lg md:text-xl pt-1 pb-4">
+          <a href={`/${slug}`}>
+            {title}
+          </a>
+        </h2>
+        <a href={`/${slug}`}>{parse(excerpt)}</a>
+      </div>
+    </article>
+  </li>
+  );
       })}
     </ul>
   );
