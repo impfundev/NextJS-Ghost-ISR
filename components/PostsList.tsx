d7ab44abd5de27728049b97da36d9f919e1b4ea7
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-27 22:25:28 +0700

    Update PostsList.tsx

--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -27,11 +27,8 @@ export default function PostsList({ posts }: Props) {
   return (
     <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 py-5">
       {posts.map((post: Post) => {
-        const { slug } = post;
         return (
-          <li key={slug}>
-            <PostCard post={post} />
-          </li>
+          <PostCard post={post} />
         );
       })}
     </ul>
