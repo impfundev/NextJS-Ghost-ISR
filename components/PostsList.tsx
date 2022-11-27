32c172eb78f6f8d7bd9249971e1e4050f4c007af
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-27 22:00:28 +0700

    Update PostsList.tsx

--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -1,8 +1,13 @@
 import { gql } from "@apollo/client";
+import { ReactNode } from "react";
 
 import PostCard from "./PostCard";
 
-export default function PostsList({ posts }) {
+interface Props {
+  posts: ReactNode;
+};
+
+export default function PostsList({ posts }: Props) {
   return (
     <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 py-5">
       {posts.map((post) => {
