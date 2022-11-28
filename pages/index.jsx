6ea7ddc621296d25c702b9d3039754d50ad8b201
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-28 16:11:33 +0700

    Update and rename index.tsx to index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,53 @@
+import { gql } from "@apollo/client";
+import Head from "next/head";
+
+import Layout from "../components/Layout";
+import PostsList from "../components/PostsList";
+import { client } from "../../lib/apolloClient";
+
+const GET_POSTS = gql`
+  query getPosts {
+    posts(first: 20, after: null) {
+      nodes {
+        title
+        slug
+        excerpt
+        featuredImage {
+          node {
+            sourceUrl
+            altText
+            sizes
+            srcSet
+          }
+        }
+      }
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const response = await client.query({
+    query: GET_POSTS,
+  });
+
+  const posts = response?.data?.posts;
+
+  return {
+    props: { posts },
+    revalidate: 1,
+  };
+}
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <Head>
+        <title>Fandomnesia</title>
+        <meta name="description" content="Fandomnesia Site" />
+      </Head>
+      <Layout>
+        <PostsList posts={posts} />
+      </Layout>
+    </>
+  );
+}
