9717d891bdbda8f44f733b2981a5d701b772c3e8
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-28 16:12:52 +0700

    Update and rename _app.tsx to _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,8 +2,7 @@ import "../styles/globals.css";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  const apolloClient = useApollo(pageProps);
-
+  
   return (
     <>
       <Head>
