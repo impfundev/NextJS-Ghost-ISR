46895f03e6e0ac19da934ddde26d57e67901ae66
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-28 21:08:56 +0700

    Update PostsList.jsx

--- components/PostsList.jsx
+++ components/PostsList.jsx
@@ -1,5 +1,6 @@
 import parse from "html-react-parser";
 import { gql } from "@apollo/client";
+import { siteUrl } from "../lib/config";
 
 export default function PostsList({ posts }) {
   return (
@@ -7,32 +8,28 @@ export default function PostsList({ posts }) {
       {posts.map((post) => {
         const { title, excerpt, slug, featuredImage } = post;
         return (
-          <li key={slug}>
+          <li key={slug}><a href={`${siteUrl}/${slug}`}>
             <article className="max-w-sm rounded-2xl shadow-lg overflow-hidden border border-black text-black hover:border-yellow-500 hover:text-yellow-500 transition-all duration-300">
-              <a className="relative" href={`/${slug}`} aria-label={title}>
-                {featuredImage ? (
-                  <>
-                    <img
-                      className="w-full object-cover h-60"
-                      src={featuredImage.node.sourceUrl}
-                      alt={featuredImage.node.altText}
-                      srcSet={featuredImage.node.srcSet}
-                      sizes={featuredImage.node.sizes}
-                      loading="lazy"
-                    />
-                  </>
-                ) : null}
-              </a>
+              {featuredImage ? (
+                <>
+                  <img
+                    className="w-full object-cover h-60"
+                    src={featuredImage.node.sourceUrl}
+                    alt={featuredImage.node.altText}
+                    srcSet={featuredImage.node.srcSet}
+                    sizes={featuredImage.node.sizes}
+                    loading="lazy"
+                  />
+                </>
+              ) : null}
               <div className="grid gap-4 m-5">
                 <h2 className="font-bold text-lg md:text-xl pt-1 pb-4">
-                  <a href={`/${slug}`}>
-                    {title}
-                  </a>
+                  {title}
                 </h2>
-                <a href={`/${slug}`}>{parse(excerpt)}</a>
+                {parse(excerpt)}
               </div>
             </article>
-          </li>
+          </a></li>
         );
       })}
     </ul>
