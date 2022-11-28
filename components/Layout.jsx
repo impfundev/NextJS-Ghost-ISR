f9251bbadad486786edaca90bd6a80e6e5cf04cf
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-28 15:58:09 +0700

    Update and rename Layout.tsx to Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,4 @@
-import { ReactNode } from "react";
 import Header from "./Header";
-
-interface Props {
-  children: ReactNode;
-};
 
 export default function Layout({ children }: Props) {
   return (
