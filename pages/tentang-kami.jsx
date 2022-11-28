2bc19b7c09c88d6a4f44bec6990e5bd24c2ec9df
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-28 21:10:33 +0700

    Update tentang-kami.jsx

--- pages/tentang-kami.jsx
+++ pages/tentang-kami.jsx
@@ -4,8 +4,8 @@ export default function Tentang() {
   
   return (
     <Layout>
-      <article className="prose lg:prose-xl">
-        <h1 className="text-xl">Tentang Kami</h1>
+      <article className="prose lg:prose-xl py-6">
+        <h1 className="text-xl py-4">Tentang Kami</h1>
         <p>Seiring meningkatnya minat banyak orang dengan dunia hiburan, kebutuhan akan informasi terkait apa yang mereka sukai juga meningkat. Disinilah Fandomnesia hadir untuk memfasilitasi banyak orang dengan informasi yang terpercaya.</p>
         <p>Fandomnesia menyajikan informasi terkini terkait dunia hiburan untuk sektor Budaya Populer yang meliputi Film, Serial Televisi, Animasi, Komik dan lainya.</p>
         <p>Kami juga membagikan informasi yang meliputi dunia para selebriti tanah air maupun luar negeri, dengan fakta berdasarkan sumber terpercaya.</p>
