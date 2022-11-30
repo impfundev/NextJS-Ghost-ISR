10abeaff86aca2967ba190ae009b9b2aaab5102e
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-30 21:43:48 +0700

    Update AdsRectangle.jsx

--- components/AdsRectangle.jsx
+++ components/AdsRectangle.jsx
@@ -2,16 +2,16 @@ import { siteUrl } from "../lib/config";
 
 export default function AdsRectangle() {
   return (
-    <div className="py-4 my-4 text-center">
-      <span className="border-b text-sm text-gray-500 border-gray-500 pb-4"> Iklan, Scroll Untuk Melanjutkan </span>
+    <div className="py-4 my-4 text-center border-y border-gray-500">
+      <span className="text-sm text-gray-500 pb-4 before:content-['Iklan, Scroll Untuk Melanjutkan']"></span>
       <img
-        className="block mx-auto"
+        className="block mx-auto py-4"
         alt="contoh iklan"
         src={`${siteUrl}/img/contoh-iklan.jpg`}
         width="300"
         height="250"
       />
-      <span className="border-t text-sm text-gray-500 border-gray-500 pt-4"> Iklan, Scroll Untuk Melanjutkan </span>
+      <span className="text-sm text-gray-500 pt-4 before:content-['Iklan, Scroll Untuk Melanjutkan']"></span>
     </div>
   );
 }
