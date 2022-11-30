8216c3fcf0e63c814096aabde2df0aa259ce3325
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-30 20:46:20 +0700

    Update AdsRectangle.jsx

--- components/AdsRectangle.jsx
+++ components/AdsRectangle.jsx
@@ -3,6 +3,7 @@ import { siteUrl } from "../lib/config";
 export default function AdsRectangle() {
   return (
     <img
+      className="block mx-auto"
       alt="contoh iklan"
       src={`${siteUrl}/img/contoh-iklan.jpg`}
       width="300"
