19f8deedbb0794a2853e3a9428ac73a5b2214a69
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-01 02:31:24 +0700

    Update FloatingAds.jsx

--- components/FloatingAds.jsx
+++ components/FloatingAds.jsx
@@ -6,7 +6,7 @@ export default function FloatingAds() {
     <button className="fixed bottom-28 right-0 p-3 bg-white border-t border-x border-inherit">⌵</button>
     <div className="floating-ads w-full h-28 bg-white border border-inherit">
       <img
-        className="block mx-auto object-cover py-3 h-28"
+        className="object-cover py-3 h-28"
         alt="contoh iklan"
         src={`${siteUrl}/img/contoh-iklan-2.png`}
         width="320"
