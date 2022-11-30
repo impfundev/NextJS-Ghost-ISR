13f939412534efb7db287c66e99214d4273a3e8c
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-01 02:02:38 +0700

    Update FloatingAds.jsx

--- components/FloatingAds.jsx
+++ components/FloatingAds.jsx
@@ -3,16 +3,16 @@ import { siteUrl } from "../lib/config";
 export default function FloatingAds() {
   return (
   <>
-    <button className="fixed bottom-28 m-3 flex items-center bg-white rounded-t-lg border-x border-t border-inherit">
-      ︿
-    </button>
-    <div className="fixed bottom-0 m-3 w-full h-auto max-h-28 bg-white border border-inherit">
+    <div className="fixed bottom-0 translate-x-1/2 w-full h-auto max-h-28 bg-white border border-inherit">
+      <button className="absolute bottom-28 p-4 flex items-center bg-white rounded-t-lg border-x border-t border-inherit">
+        <span>⌵</span>
+      </button>
       <img
-        className="block mx-auto w-full h-auto object-cover"
+        className="block mx-auto w-full h-28 object-cover px-4 pb-4"
         alt="contoh iklan"
         src={`${siteUrl}/img/contoh-iklan-2.png`}
         width="320"
-        height="50"
+        height="100"
       />
     </div>
   </>
