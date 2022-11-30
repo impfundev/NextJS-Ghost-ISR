c267b70472c838dd434ebd59f6f95ba19e268537
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-11-30 16:10:03 +0700

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { siteUrl } from "../lib/config";
 
 export default function Document() {
   return (
     <Html lang="id">
       <Head>
+        <link type="application/rss+xml" rel="alternate" title="Fandomnesia" href={`${siteUrl}/feed.xml`} />
         <meta name="robots" content="noindex" />
       </Head>
       <body>
