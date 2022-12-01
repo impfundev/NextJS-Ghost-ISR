eb013e3bee3847b77887e51792b1cbae9736ce35
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-01 15:52:38 +0700

    Update FloatingAds.jsx

--- components/FloatingAds.jsx
+++ components/FloatingAds.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import { siteUrl } from "../lib/config";
 
 export default function FloatingAds() {
+  const [adsActive, setAdsActive] = useState(false);
+  const handleAds = () => {
+    setAdsActive(!adsActive);
+  };
+
   return (
   <>
-    <button className="fixed bottom-20 right-0 p-1 bg-white rounded-t-lg border-t border-x border-inherit">⌵</button>
-    <div className="fixed bottom-0 w-full h-20 bg-white border border-inherit">
+    <button aria-label="Floating Ads" onClick={handleAds} className="fixed bottom-20 right-0 p-1 bg-white font-medium rounded-t-lg border-t border-x border-inherit">
+      { adsActive ? (
+        <span>﹀</span>
+      ) : (
+        <span>︿</span>
+      )}
+    </button>
+    <div className={`fixed bottom-0 w-full ${ navActive ? 'h-20' : 'h-0' } bg-white border border-inherit transition-all ease-in-out duration-300`}>
       <img
         className="block mx-auto object-cover py-2 w-[320px] h-auto"
         alt="contoh iklan"
