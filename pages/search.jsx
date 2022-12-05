3db4e8b9a8ce7d3f93a4280875ce1e449e9f0ee9
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-05 22:21:29 +0700

    Update search.jsx

--- pages/search.jsx
+++ pages/search.jsx
@@ -21,11 +21,10 @@ export default function Search() {
             id="search-input"
             type="text"
             name="q"
-            value="site%3Afandomnesia.com+"
             className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Mulai ketik sesuatu disini..."
           />
-          <button type="submit" value="search" formtarget="_blank" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
+          <button type="submit" value="site%3Afandomnesia.com+" formtarget="_blank" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
         </div>
       </form>
       </article>
