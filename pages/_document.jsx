de4fcc363f4a49b92bcd785054d3b893a0c00873
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-11 01:47:57 +0700

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,34 +1,8 @@
-import { NextSeo } from "next-seo";
 import { Html, Head, Main, NextScript } from 'next/document'
-import { siteUrl } from "../lib/config";
 
 export default function Document() {
   return (
     <Html lang="id">
-        <NextSeo
-          noindex={true}
-          nofollow={true}
-          robotsProps={{
-            maxImagePreview: 'large',
-          }}
-          additionalLinkTags={[
-            {
-              rel: 'icon',
-              href: `${siteUrl}/favicon.png`,
-            },
-            {
-              rel: 'apple-touch-icon',
-              href: `${siteUrl}/favicon.png`,
-              sizes: '150x150'
-            },
-            {
-              type: 'application/rss+xml',
-              rel: 'alternate',
-              title: 'Fandomnesia',
-              href: `${siteUrl}/feed`
-            },
-          ]}
-        />
       <Head />
       <body>
         <Main />
