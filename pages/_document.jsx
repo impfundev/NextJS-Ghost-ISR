589b2716bd72c8ff9afa06221fb7e25d710a6855
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-10 22:28:34 +0700

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,8 +5,26 @@ export default function Document() {
   return (
     <Html lang="id">
       <Head>
-        <link type="application/rss+xml" rel="alternate" title="Fandomnesia" href={`${siteUrl}/feed`} />
-        <meta name="robots" content="noindex" />
+        <NextSeo
+          noindex={true}
+          nofollow={true}
+          robotsProps={{
+            maxImagePreview: 'large',
+          }}
+          additionalLinkTags={[
+            {
+              rel: 'apple-touch-icon',
+              href: `${siteUrl}/favicon.png`,
+              sizes: '150x150'
+            },
+            {
+              type: 'application/rss+xml',
+              rel: 'alternate',
+              title: 'Fandomnesia',
+              href: `${siteUrl}/feed`
+            },
+          ]}
+        />
       </Head>
       <body>
         <Main />
