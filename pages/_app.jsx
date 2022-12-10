20992c74238f3384da04e1c295cbff00a63fcd65
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-11 01:48:43 +0700

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,6 @@
 import { Rubik } from "@next/font/google";
+import { NextSeo } from "next-seo";
+import { siteUrl } from "../lib/config";
 import "../styles/globals.css";
 
 const rubik = Rubik({ subsets: ['latin'] });
@@ -7,6 +9,30 @@ function MyApp({ Component, pageProps }) {
   
   return (
     <>
+        <NextSeo
+          noindex={true}
+          nofollow={true}
+          robotsProps={{
+            maxImagePreview: 'large',
+          }}
+          additionalLinkTags={[
+            {
+              rel: 'icon',
+              href: `${siteUrl}/favicon.png`,
+            },
+            {
+              rel: 'apple-touch-icon',
+              href: `${siteUrl}/favicon.png`,
+              sizes: '150x150'
+            },
+            {
+              type: 'application/rss+xml',
+              rel: 'alternate',
+              title: 'Fandomnesia',
+              href: `${siteUrl}/feed`
+            },
+          ]}
+        />
       <style jsx global>{`
         html {
           font-family: ${rubik.style.fontFamily};
