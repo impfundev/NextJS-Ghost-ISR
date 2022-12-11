2a9502a439e6e814bfa7b8d4a97590133783ec99
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-12 01:36:07 +0700

    Update PostsList.jsx

--- components/PostsList.jsx
+++ components/PostsList.jsx
@@ -10,16 +10,15 @@ export default function PostsList({ posts, thumbnail }) {
           <li key={id}><a href={`${siteUrl}/${slug}`}>
             <article className="max-w-sm rounded-2xl shadow-lg overflow-hidden border border-black text-black hover:border-yellow-500 hover:text-yellow-500 transition-all duration-300">
               {feature_image ? (
-                <figure className="relative w-[590px] h-60">
-                  <Image
-                    src={thumbnail.url}
-                    width={thumbnail.width}
-                    height={thumbnail.height}
-                    alt={title}
-                    layout="responsive"
-                    quality={70}
-                  />
-                </figure>
+                <Image
+                  className="object-cover w-full h-60"
+                  src={thumbnail.url}
+                  width={thumbnail.width}
+                  height={thumbnail.height}
+                  alt={title}
+                  layout="responsive"
+                  quality={70}
+                />
               ) : null}
               <div className="grid gap-4 m-5">
                 <h2 className="font-bold text-lg md:text-xl pt-1 pb-4">
