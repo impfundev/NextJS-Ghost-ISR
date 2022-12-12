7db2f89a9cff02fe60b7e7c168bcc744a0c217a1
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-12 17:05:06 +0700

    Update MorePost.jsx

--- components/MorePost.jsx
+++ components/MorePost.jsx
@@ -1,15 +1,12 @@
-import LazyLoad from "react-lazy-load";
 import PostsList from "./PostsList";
 
 export default function MorePost({ posts, thumbnail }) {
   return (
     <>
       <h3 className="text-lg font-bold py-4">Artikel Terbaru</h3>
-      <LazyLoad threshold={0.95}>
-        <PostsList
-          posts={posts}
-        />
-      </LazyLoad>
+      <PostsList
+        posts={posts}
+      />
     </>
   );
 }
