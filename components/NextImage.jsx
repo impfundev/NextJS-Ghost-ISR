2650097e903935c9649c3c11e975c655541ae617
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-12 16:44:55 +0700

    Create NextImage.jsx

---
+++ components/NextImage.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import probe from "probe-image-size";
+
+export async function NextImage({ image }) {
+  let thumbnail = await probe(image, { rejectUnauthorized: false });
+ 
+  return (
+    <Image
+      className="object-cover w-full h-60"
+      src={thumbnail.url}
+      width={thumbnail.width}
+      height={thumbnail.height}
+      alt={title}
+      layout="responsive"
+      quality={70}
+    />
+  );
+}
+
+export default function NextImage;
