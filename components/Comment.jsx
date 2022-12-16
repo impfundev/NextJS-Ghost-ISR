c4d5e7cbe83630a5620171f75136805e141dc99a
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-17 05:00:47 +0700

    Update Comment.jsx

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,26 +1,19 @@
-import Head from "next/head";
+import Script from "next/script";
 
 export default function Comment({ url }) {
   return (
     <>
-    <Head>
-      <script
-        dangerouslySetInnerHTML={{
-        __html: `let lzAd = false;
-          window.addEventListener('scroll', () => {
-            (0 != document.documentElement.scrollTop && false === lzAd || 0 != document.body.scrollTop && !1 === lzAd) && (!function(){
-              itemScript = document.createElement('script');
-              itemScript.src= 'https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v15.0';
-              itemScript.async = true;
-              itemScript.setAttribute('crossorigin', 'anonymous');
-              itemScript.setAttribute('nonce', 'HZqJ51n7');
-              itemScript.setAttribute('data-numposts', '5');
-              document.head.appendChild(itemScript);
-            }(), lzAd = true);
-          }, true);`
-        }}
+      <Script
+        src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v15.0"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, FB Comment has been populated`)
+        }
+        async="true"
+        crossorigin="anonymous"
+        nonce="HZqJ51n7"
+        data-numposts="5"
       />
-    </Head>
     <div className="py-5">
       <div className="fb-comments" data-href={url} data-width="100%" data-numposts="5"></div>
     </div>
