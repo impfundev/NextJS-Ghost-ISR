ec2c203826ecd7a21d251c84c993c47ab5915bfe
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-18 03:20:09 +0700

    Update Comment.jsx

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,23 +1,11 @@
-import Script from "next/script";
+import { FacebookProvider, Comments } from "react-facebook";
 
 export default function Comment({ url }) {
   return (
     <>
-      <Script
-        src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v15.0"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, FB Comment has been populated`)
-        }
-        async="true"
-        crossorigin="anonymous"
-        nonce="HZqJ51n7"
-        data-numposts="5"
-      />
-    <div className="py-5">
-      <div className="fb-comments" data-href={url} data-width="100%" data-numposts="5"></div>
-    </div>
-    <div id="fb-root"></div>
-  </>
+      <FacebookProvider appId="5628930273871755">
+        <Comments href={url} />
+      </FacebookProvider>
+    </>
   );
 }
