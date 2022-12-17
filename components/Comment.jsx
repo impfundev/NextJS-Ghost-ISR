1c4a690cd2b99c0c1365000f222224c214b21457
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-18 03:57:33 +0700

    Update Comment.jsx

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -4,12 +4,6 @@ export default function Comment({ url }) {
   return (
     <>
       <div class="fb-comments" data-href={url} data-width="100%" data-numposts="5"></div>
-      <div id="fb-root"></div>
-      <Script
-        src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v15.0&appId=5628930273871755&autoLogAppEvents=1"
-        strategy="lazyOnload"
-        nonce="8IcioGBr"
-      />
     </>
   );
 }
