155e9e3e95276df56ce10db91c8f6ec1128f14cd
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-19 04:48:46 +0700

    Update Thumbnail.jsx

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -11,6 +11,13 @@ export default function Thumbnail({ thumbnail, caption, title }) {
         layout="responsive"
         quality={80}
         priority
+        sizes={`
+          (max-width: 600px) 480px,
+          (max-width: 700px) 640px,
+          (max-width: 1000px) 760px,
+          (max-width: 1900px) 1080px,
+          (max-width: 2000px) 1200px, ${thumbnail.width}
+        `}
       />
       {caption ? (
         <figcaption className="py-0">{caption}</figcaption>
