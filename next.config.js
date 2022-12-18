76e495f1a5cb5743e0501bc103e5be62b8556c50
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-19 04:33:12 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'max-age=31536000',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+        ],
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
