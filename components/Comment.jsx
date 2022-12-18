a3e92da173cb259093ace41d51d63fe73d2ea954
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-19 03:19:30 +0700

    Update Comment.jsx

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -6,10 +6,28 @@ export default function Comment({ url }) {
       <div class="fb-comments" data-href={url} data-width="100%" data-numposts="5"></div>
       <div id="fb-root"></div>
       <Script
-        src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v15.0&appId=5628930273871755&autoLogAppEvents=1"
         strategy="lazyOnload"
-        nonce="8IcioGBr"
-        crossorigin="anonymous"
+        dangerouslySetInnerHTML={{
+          __html: `
+            let lzAd = false;
+            window.addEventListener('scroll', () => {
+              (0 != document.documentElement.scrollTop && false === lzAd || 0 != document.body.scrollTop && !1 === lzAd) && (!function(){
+                fbComment = document.createElement('script');
+                fbComment.src = 'https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v15.0&autoLogAppEvents=1';
+                fbComment.async = true;
+                fbComment.setAttribute('crossorigin', 'anonymous');
+                fbComment.setAttribute('nonce', '8IcioGBr');
+                document.head.appendChild(fbComment);
+              }(), lzAd = true);
+            }, true);
+          `,
+        }}
+        onLoad={() => {
+          console.log('Lazy Script has loaded')
+        }}
+        onError={(e) => {
+          console.error('Lazy Script failed to load', e)
+        }}
       />
     </>
   );
