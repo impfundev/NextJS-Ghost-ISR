895c707cfed3eab7d7ca741ace68e69086625119
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-19 06:16:59 +0700

    Update NextImage.jsx

--- components/NextImage.jsx
+++ components/NextImage.jsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
+import probe from "probe-image-size";
 
-export default function NextImage({ image, title }) {
-  
+async function NextImage({ image, title }) {
+  let thumbnail = await probe(image, { rejectUnauthorized: false });
+
   return (
     <Image
       className="object-cover w-full h-60"
-      src={image}
-      width={480}
-      height={130}
+      src={thumbnail.url}
+      width={thumbnail.width}
+      height={thumbnail.height}
       alt={title}
       layout="responsive"
       quality={70}
-      sizes={`(max-width: 640px) 480px, (max-width: 1080px) 828px, (max-width: 2048px) 1200px, 480px`}
+      sizes={`(max-width: 640px) 480px, (max-width: 1080px) 828px, (max-width: 2048px) 1200px, ${thumbnail.width}px`}
     />
   );
 };
+
+export default function NextImage;
