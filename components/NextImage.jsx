dab05cbe160abfc2a6ad2e82a53da7c78e0df6ea
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-19 06:53:58 +0700

    Update NextImage.jsx

--- components/NextImage.jsx
+++ components/NextImage.jsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function NextImage({ image, title }) {
 
   return (
-    <div style={{ position: 'relative', width: '480px', height: '240px' }}>
-    <Image
-      className="object-cover"
-      src={image}
-      alt={title}
-      fill
-      sizes="100vw"
-    />
+    <div style={{ position: 'relative', width: '384px', height: '240px' }}>
+      <Image
+        className="object-cover"
+        src={image}
+        alt={title}
+        fill
+        sizes="100vw"
+      />
     </div>
   );
 };
