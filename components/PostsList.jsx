48de4f8e7170831247b4be3c1e9812a824e1cd21
fandomnesia <93136927+fandomnesia@users.noreply.github.com>
2022-12-19 07:30:30 +0700

    Update PostsList.jsx

--- components/PostsList.jsx
+++ components/PostsList.jsx
@@ -8,7 +8,7 @@ export default function PostsList({ posts }) {
         const { id, title, excerpt, slug, feature_image } = post;
         return (
           <li key={id}><a href={`${siteUrl}/${slug}`}>
-            <article className="max-w-sm rounded-2xl shadow-lg overflow-hidden border border-black text-black hover:border-yellow-500 hover:text-yellow-500 transition-all duration-300">
+            <article className="relative max-w-sm rounded-2xl shadow-lg overflow-hidden border border-black text-black hover:border-yellow-500 hover:text-yellow-500 transition-all duration-300">
               {feature_image ? (
                 <NextImage
                   image={feature_image}
